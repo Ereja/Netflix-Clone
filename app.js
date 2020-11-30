d7a2b0244c09d92b642883e629bb82d4b82219c1
Vaida <93vaida@gmail.com>
2020-11-30 15:45:02 +0100

    FAQ accordion script

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//selecting all question elements
+const questions = document.querySelectorAll(".question");
+
+//looping through questions in order to access the buttons
+questions.forEach(question => {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", () => {
+    //checking if any item is already selected, if so their class is being removed ensuring that on press of new question, the older one closes
+    questions.forEach(item => {
+      if (item !== question) {
+        item.classList.remove("display-answer");
+      }
+    });
+    question.classList.toggle("display-answer");
+  });
+});
